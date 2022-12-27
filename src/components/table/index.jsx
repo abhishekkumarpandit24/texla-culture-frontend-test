5d72ce8c91c6ff916715ad65c55abd7df59125da
AbhishekPandit24 <abhishek_pandit@persistent.com>
2022-12-27 14:47:32 +0530

    second commit

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,104 +1,34 @@
-import { Box, Paper, Typography } from "@mui/material";
-import { DataGrid } from "@mui/x-data-grid";
-import { mockDataTeam } from "../../data/mockData";
-import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
-import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
-import TableHeader from "./TableHeader";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { useDemoData } from '@mui/x-data-grid-generator';
+import { DataGrid } from '@mui/x-data-grid';
+import { mockDataTeam } from '../../data/mockData';
+import TableHeader from './TableHeader'
+import { Paper } from '@mui/material';
 
-const Table = () => {
-  const columns = [
-    { field: "id", headerName: "ID" },
-    {
-      field: "name",
-      headerName: "Name",
-      flex: 1,
-      cellClassName: "name-column--cell",
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      headerAlign: "left",
-      align: "left",
-    },
-    {
-      field: "email",
-      headerName: "Email",
-      flex: 1,
-    },
-    {
-      field: "phone",
-      headerName: "Phone Number",
-      flex: 1,
-    },
-    {
-      field: "accessLevel",
-      headerName: "Status",
-      flex: 1,
-      renderCell: ({ row: { access } }) => {
-        return (
-          <Box
-            width="80%"
-            m="auto"
-            p="5px"
-            display="flex"
-            justifyContent="center"
-            backgroundColor={
-              access === "admin"
-                ? "green"
-                : access === "manager"
-                ? "green"
-                : "red"
-            }
-            borderRadius="4px"
-          >
-            {access === "admin" && <AdminPanelSettingsOutlinedIcon />}
-            {access === "manager" && <SecurityOutlinedIcon />}
-            {access === "user" && <LockOpenOutlinedIcon />}
-            <Typography>{access}</Typography>
-          </Box>
-        );
-      },
-    },
-  ];
+
+export default function CustomFooter() {
+  const { data } = useDemoData({
+    dataSet: mockDataTeam,
+    rowLength: 10,
+    maxColumns: 10,
+  });
 
   return (
     <Box>
 
     <TableHeader/>
-    <Paper elevation={4} sx={{m:"30px", borderRadius: "0px 0px 20px 20px"}}>
-      <Box
-        m="40px 0 0 0"
-        height="75vh"
-        sx={{
-          "& .MuiDataGrid-root": {
-            border: "none",
-            fontSize: "20px"
-          },
-          "& .MuiDataGrid-cell": {
-            borderBottom: "none",
-          },
-          "& .name-column--cell": {
-            color: "gray",
-          },
-          "& .MuiDataGrid-columnHeaders": {
-            borderBottom: "none",
-          },
-          "& .MuiDataGrid-virtualScroller": {},
-          "& .MuiDataGrid-footerContainer": {
-            borderTop: "none",
-          },
-          "& .MuiCheckbox-root": {
-            color: `gray !important`,
-          },
-        }}
-      >
-        <DataGrid checkboxSelection rows={mockDataTeam} columns={columns} />
+    <Paper elevation={2} sx={{m:"30px", borderRadius: "0px 0px 20px 20px"}}>
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ height: 350, width: '100%', mb: 1 }}>
+        <DataGrid
+          {...data}
+          
+        />
       </Box>
+      
+    </Box>
     </Paper>
     </Box>
   );
-};
-
-export default Table;
+}
